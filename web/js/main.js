1b5dbf95b7a5330a89b5d0bc8fa0a3e5568993c6
Elvis Jr Crego <elvis.crego@gmail.com>
2019-01-28 18:01:38 -0500

    Cambios ariel

--- web/js/main.js
+++ web/js/main.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
 
 
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+
  $("#owl-example").owlCarousel({
     // Most important owl features
     items : 4,
